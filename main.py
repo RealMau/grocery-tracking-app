8c3489caf67673252b7ea4736ee13451f17094e4
Mauro <mauromonasterio04@gmail.com>
2024-02-06 17:19:50 -0300

    inicializada la instancia de la app, a partid de aca hay que construir.

---
+++ main.py
@@ -0,0 +1,11 @@
+# En este archivo va lo principal de la app.
+
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+class MyApp(App):
+    def build(self):
+        return Label(text='Hello world')
+
+if __name__ == '__main__':
+    MyApp().run()
